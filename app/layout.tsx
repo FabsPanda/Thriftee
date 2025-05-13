d8c780acfbcd4d52244b430b198525bd92a90e9f
louiskrisdy <louiskrisdy@gmail.com>
2025-05-13 13:29:10 +0700

    adding footer (need fixing: spacing to content, dark mode footer color)

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,17 +39,29 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <Script src="https://app.sandbox.midtrans.com/snap/snap.js" data-client-key={process.env.NEXT_PUBLIC_CLIENT_KEY} strategy="afterInteractive"></Script>
-      <body suppressHydrationWarning
-        className={roboto.className}
-      >
+      <body suppressHydrationWarning className={cn("min-h-screen flex flex-col", roboto.className)}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex-grow px-6 md:px-12 mx-auto max-w-8xl">
+          <div className="flex-grow px-6 md:px-12 mx-auto w-full max-w-8xl">
             <Nav />
             <Toaster />
             {children}
           </div>
+
+          <footer className="bg-primary rounded-t-lg shadow-sm dark:bg-gray-800 w-full">
+            <div className="mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+              <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+                © 2025 <a href="/" className="hover:underline">Thriftee</a>. All Rights Reserved.
+              </span>
+              <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li><a href="#" className="hover:underline me-4 md:me-6">About</a></li>
+                <li><a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a></li>
+                <li><a href="#" className="hover:underline">Contact</a></li>
+              </ul>
+            </div>
+          </footer>
         </ThemeProvider>
       </body>
+
     </html>
   );
 }
